051bff34917772b2441afc93e8c63d97d78482d3
Arthur Land Avila <arthurlavila2000@gmail.com>
2026-07-16 11:05:53 -0300

    Exercício 5

--- projeto/app.py
+++ projeto/app.py
@@ -2,6 +2,53 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+usuarios = []
+
+@app.route("/cadastro", methods=["GET", "POST"])
+def cadastro():
+
+    if request.method == "POST":
+
+        usuario = request.form["usuario"]
+        email = request.form["email"]
+        senha = request.form["senha"]
+        nascimento = request.form["nascimento"]
+
+        usuarios.append({
+            "usuario": usuario,
+            "email": email,
+            "senha": senha,
+            "nascimento": nascimento
+        })
+
+        return render_template(
+            "login.html",
+            mensagem="Usuário cadastrado com sucesso! Faça seu login."
+        )
+
+    return render_template("cadastro.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        usuario = request.form["usuario"]
+        senha = request.form["senha"]
+
+        for pessoa in usuarios:
+
+            if pessoa["usuario"] == usuario and pessoa["senha"] == senha:
+                return render_template("home.html")
+
+        return render_template(
+            "login.html",
+            mensagem="Usuário ou senha inválidos!"
+        )
+
+    return render_template("login.html")
+
+
 @app.route("/enviar", methods=["POST"])
 def enviar():
     return render_template("sucesso.html")
@@ -13,13 +60,6 @@ def index():
 @app.route("/formulario")
 def formulario():
     return render_template("formulario.html")
-
-@app.route("/enviar", methods=["POST"])
-def enviar():
-    nome = request.form["nome"]
-    email = request.form["email"]
-
-    return f"Nome: {nome}<br>E-mail: {email}"
 
 if __name__ == "__main__":
     app.run(debug=True)
